87187fa12fee19c5decb2643d37e74bc74a22370
PKC278 <52959804+PKC278@users.noreply.github.com>
2022-07-24 14:40:23 +0800

    Update setPROXY.sh

--- setPROXY.sh
+++ setPROXY.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+ #!/bin/bash
 
 myproxy="socks5://user:364720@nas.pkcpersonal.cn:5720"
 
+echo -e "curl and git's proxy will set to \033[1m$myproxy\033[0m"
+
+#set proxy for current user's curl
 echo "proxy=\"$myproxy\"" > $HOME/.curlrc
+#set proxy for root's curl
 echo "proxy=\"$myproxy\"" > /root/.curlrc
 
+set proxy for git
 git config --global http.proxy $myproxy
