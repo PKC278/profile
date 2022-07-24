a1864a9c412beb77d5c342a1a41c0760b7456f5e
PKC278 <52959804+PKC278@users.noreply.github.com>
2022-07-24 15:14:17 +0800

    Delete setPROXY.sh

--- setPROXY.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-myproxy="socks5://user:364720@nas.pkcpersonal.cn:5720"
-echo "proxy=\"$myproxy\"" > $HOME/.curlrc
-echo "proxy=\"$myproxy\"" > /root/.curlrc
-git config --global http.proxy $myproxy
