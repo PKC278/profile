d9b0a7af3625b0116dfdfb07d8e6042089e12412
PKC278 <52959804+PKC278@users.noreply.github.com>
2022-07-25 19:38:44 +0800

    Update setProxy.sh

--- setProxy.sh
+++ setProxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-myproxy="socks5://user:364720@nas.pkcpersonal.cn:5720"
+myproxy="socks5://nas.pkcpersonal.cn:5720"
 
 echo "proxy=\"$myproxy\"" > $HOME/.curlrc
 echo "proxy=\"$myproxy\"" > /root/.curlrc
