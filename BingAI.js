f20855c19ed2812aa2ba9e24f2f0f43d2060bde7
PKC278 <52959804+PKC278@users.noreply.github.com>
2023-07-29 16:40:56 +0800

    Update BingAI.js

--- BingAI.js
+++ BingAI.js
@@ -1,7 +1,12 @@
 const headers = $request.headers;
 console.log(headers);
-delete headers["user-agent"];
 
-headers['User-Agent'] = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51`;
+if (headers["user-agent"].includes("Mobile")) {
+  console.log("User-Agent contains 'Mobile'. Skipping the code.");
+  $done({ headers });
+} else {
+  delete headers["user-agent"];
+  headers['User-Agent'] = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51`;
 
-$done({ headers });
+  $done({ headers });
+}
