6d25df54d07d80c30ad27f1959de5885f22d7779
PKC278 <52959804+PKC278@users.noreply.github.com>
2023-07-29 11:10:40 +0800

    Update BingAI.js

--- BingAI.js
+++ BingAI.js
@@ -1,14 +1,17 @@
 const headers = $request.headers;
+const originalUA = headers['User-Agent'];
 
-delete headers["user-agent"];
-delete headers["sec-ch-ua-full-version"];
-delete headers["sec-ch-ua-full-version-list"];
-let ua = 'iPhone';
+if (originalUA.includes('iPhone')) {
+  // 如果原始 User-Agent 包含 'iPhone'，则认为是 iPhone 设备
+  headers['User-Agent'] = 'Mozilla/5.0 (${ua}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41';
+} else if (originalUA.includes('Macintosh')) {
+  // 如果原始 User-Agent 包含 'Macintosh'，则认为是 macOS 设备
+  headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51';
+}
 
-
-headers['User-Agent'] = `Mozilla/5.0 (${ua}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41`;
-headers['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
-headers['sec-ch-ua-mobile'] = '?0'
-headers['sec-ch-ua-platform'] = 'macOS'
+// 添加其他请求头部信息
+headers['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"';
+headers['sec-ch-ua-mobile'] = '?0';
+headers['sec-ch-ua-platform'] = 'macOS';
 
 $done({ headers });
