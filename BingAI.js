8a14f785089af828865e42e04dd3781741beddd1
PKC278 <52959804+PKC278@users.noreply.github.com>
2023-10-02 22:26:14 +0800

    Update BingAI.js

--- BingAI.js
+++ BingAI.js
@@ -4,9 +4,8 @@ if (headers["user-agent"].includes("Mobile")) {
   delete headers["user-agent"];
   delete headers["sec-ch-ua-full-version"];
   delete headers["sec-ch-ua-full-version-list"];
-  let ua = 'iPhone';
 
-  headers['User-Agent'] = `Mozilla/5.0 (${ua}; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41`;
+  headers['User-Agent'] = `Mozilla/5.0 (iPhone; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.51`;
   headers['sec-ch-ua'] = '"Chromium";v="110", "Not A(Brand";v="24", "Microsoft Edge";v="110"'
   headers['sec-ch-ua-mobile'] = '?0'
   headers['sec-ch-ua-platform'] = 'macOS'
